5482183573ab9c67f874d29f0587b622cf4ba29a
Ara-lee <tomibentayo@gmail.com>
2022-02-11 11:57:51 +0100

    Initial commit

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+typeof "man" //String
+typeof 5 //Number
+typeof null //Object
+typeof false //Boolean
+
+let carColor;
+let houseAddress;
+let office;
+let animal;
+
+const age = 20;
+const birthMonth = 'December';
+const officeAddress = false;
+const accountBalance = null;
+
+var firstName = 'Adedoyin';
+var lastName = 'Awotayo';
+var maritalStatus = null;
+var country = 'Nigeria'; //sadly
+var time = 25;
+
+let myAge = 'i am 25years old';
+let yourAge = 'you are 35years old';
+
+console.log(myAge)
+console.log(yourAge)
+
---
+++ variable.js
@@ -0,0 +1,14 @@
+//comments can make code readable
+
+//Welcome to 30DaysOfJavaScript
+
+/**
+comments can make code readable
+easy to reuse,
+and informative
+ */
+
+var name = `Adedoyin`; //String
+var Age = false; //Boolean
+var sex; //Undefined
+var height = null; //Null
